0a97c9ff2f9af244da1ebfb02f38167c1ce5eb0b
MustapheMS <moztaphem9@gmail.com>
2023-06-16 12:19:20 +0300

    Zero & One Done 100%

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Good Luck 💪🏾
+
+//Done 💯💯💯
+
+function ZeroAndOne(arr) {
+    let result = '';
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === 'Zero') {
+        result += '0';
+      } else if (arr[i] === 'One') {
+        result += '1';
+      }
+    }
+    return result;
+  }
+  console.log(ZeroAndOne(["one","Zero","Zero","One"])); 
+  console.log(ZeroAndOne(["Zero", "ONE", "one", "Zero"])); 
+  console.log(ZeroAndOne(["Zero","One","ONE","one","Zero","ZERO","zero","One"]))
